3f05c3994180e5a8c024b51db709e8ad845107d3
Kevin <kevinm1776@gmail.com>
2020-01-22 15:17:19 -0800

    Add size property.

--- Code/linkedlist.py
+++ Code/linkedlist.py
@@ -104,6 +104,7 @@ class LinkedList(object):
             self.tail.next = new_node
         # Update tail to new node regardless
         self.tail = new_node
+        self.size += 1
 
     def prepend(self, item):
         """Insert the given item at the head of this linked list.
@@ -189,6 +190,9 @@ class LinkedList(object):
                     previous.next = None
                 # Update tail to the previous node regardless
                 self.tail = previous
+
+            self.size -= 1
+
         else:
             # Otherwise raise an error to tell the user that delete has failed
             raise ValueError('Item not found: {}'.format(item))
