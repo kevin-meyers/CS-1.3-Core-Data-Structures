ac898cba187603aae4e056f43707e6a5d6c489f9
Kevin <kevinm1776@gmail.com>
2020-01-27 15:15:01 -0800

    Improve functions.

--- Code/bases.py
+++ Code/bases.py
@@ -1,16 +1,8 @@
-#!python
-
 import string
-# Hint: Use these string constants to encode/decode hexadecimal digits and more
-# string.digits is '0123456789'
-# string.hexdigits is '0123456789abcdefABCDEF'
-# string.ascii_lowercase is 'abcdefghijklmnopqrstuvwxyz'
-# string.ascii_uppercase is 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-# string.ascii_letters is ascii_lowercase + ascii_uppercase
-# string.printable is digits + ascii_letters + punctuation + whitespace
+
+
 BASE_ENCODE = string.digits + string.ascii_lowercase
 BASE_DECODE = {digit: val for val, digit in enumerate(BASE_ENCODE)}
-
 
 
 def decode(digits, base):
@@ -23,37 +15,20 @@ def decode(digits, base):
     # Implement decode - decode digits in any base to a number
     decoded = 0
     for pos, digit in enumerate(reversed(digits)):
-        decoded += base**pos*BASE_DECODE[digit]
+        decoded += pow(base,pos) * BASE_DECODE[digit]
 
     return decoded
 
 
-def encode(number, base):
-    """Encode given number in base 10 to digits in given base.
-    number: int -- integer representation of number (in base 10)
-    base: int -- base to convert to
-    return: str -- string representation of number (in given base)"""
-    # Handle up to base 36 [0-9a-z]
-    assert 2 <= base <= 36, 'base is out of range: {}'.format(base)
-    # Handle unsigned numbers only for now
-    assert number >= 0, 'number is negative: {}'.format(number)
-    encoded = ''
-    highest_power = 1
-    multiple = base
-    while multiple <= number:
-        multiple *= base
-        highest_power += 1
 
-    for power in reversed(range(highest_power)):
-        multiple = base ** power
-        if multiple > number:
-            encoded += '0'
+def encode(number, base):  # Bens godlike encode
+    if number < base:
+        return BASE_ENCODE[number]
 
-        else:
-            div, number = divmod(number, multiple)
-            encoded += str(BASE_ENCODE[div])
+    div, mod = divmod(number, base)
 
-    return encoded
+    return encode(div, base) + BASE_ENCODE[mod]
+
 
 def convert(digits, base1, base2):
     """Convert given digits in base1 to digits in base2.
@@ -86,3 +61,34 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+
+def old_encode(number, base):
+    """Encode given number in base 10 to digits in given base.
+    number: int -- integer representation of number (in base 10)
+    base: int -- base to convert to
+    return: str -- string representation of number (in given base)"""
+    # Handle up to base 36 [0-9a-z]
+    assert 2 <= base <= 36, 'base is out of range: {}'.format(base)
+    # Handle unsigned numbers only for now
+    assert number >= 0, 'number is negative: {}'.format(number)
+    encoded = ''
+    highest_power = 1
+    multiple = base
+    while multiple <= number:
+        multiple *= base
+        highest_power += 1
+
+    for power in reversed(range(highest_power)):
+        multiple = base ** power
+        if multiple > number:
+            encoded += '0'
+
+        else:
+            div, number = divmod(number, multiple)
+            encoded += str(BASE_ENCODE[div])
+
+    return encoded
+
+
