edc20964d9e678e2052bda177b8f63e408b9b789
Kevin <kevinm1776@gmail.com>
2020-03-10 00:35:45 -0400

    Created DefaultTable, to mimic defaultdict behaviour.

--- Code/utils.py
+++ Code/utils.py
@@ -1,3 +1,14 @@
+from copy import copy
+
 from hashtable import HashTable
 
 
+class DefaultTable(HashTable):
+    def __init__(self, default):
+        self.default = lambda: copy(default)
+        super().__init__()
+
+    def __missing__(self, key):
+        self[key] = self.default()
+
+        return self[key]
